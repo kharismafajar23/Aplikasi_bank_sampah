157cf4bd6dac794dbde373e61e6360bcd22be520
kharismafajar23 <70358725+kharismafajar23@users.noreply.github.com>
2022-01-03 14:00:10 +0700

    bau pokonya

---
+++ app/src/main/java/com/demanganesia/banksampah/DetailBankSampahActivity.java
@@ -0,0 +1,14 @@
+package com.demanganesia.banksampah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailBankSampahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_bank_sampah);
+    }
+}
