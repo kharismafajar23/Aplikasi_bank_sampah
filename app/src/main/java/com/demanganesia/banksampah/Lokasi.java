6a09d622a241fea96998dab556a1d0d0b14fc712
kharismafajar23 <70358725+kharismafajar23@users.noreply.github.com>
2022-01-04 20:17:46 +0700

    maps bisa

--- app/src/main/java/com/demanganesia/banksampah/Lokasi.java
+++ app/src/main/java/com/demanganesia/banksampah/Lokasi.java
@@ -1,6 +1,7 @@
 package com.demanganesia.banksampah;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +10,17 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Lokasi extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Lokasi extends FragmentActivity implements OnMapReadyCallback {
 
     ImageButton btnBack;
+    GoogleMap maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,10 @@ public class Lokasi extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_lokasi);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
         btnBack = findViewById(R.id.btnBack1);
+
 
         //kembali
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +40,15 @@ public class Lokasi extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        maps = googleMap;
+
+        LatLng BSNogolaten = new LatLng(-7.7806991, 110.4000235);
+        maps.addMarker(new MarkerOptions().position(BSNogolaten).title("Bank Sampah Nogolaten"));
+        maps.moveCamera(CameraUpdateFactory.newLatLng(BSNogolaten));
     }
 }
